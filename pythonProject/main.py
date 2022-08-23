fc23121fdcc2adf82873b4193a36f3bb355a5554
jongwon_kim <jongwon_kim@tmax.co.kr>
2022-08-23 20:09:47 +0900

    to_sql로 upload 구현

--- pythonProject/main.py
+++ pythonProject/main.py
@@ -6,10 +6,9 @@ import sqlite3 as sql
 from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite://'
 app.config['SECRET_KEY'] = "random string"
 
-db = SQLAlchemy(app)
 engine = create_engine('sqlite://', echo=False)
 
 
@@ -23,53 +22,9 @@ def upload():
     if request.method == "GET":
         return render_template("upload.html")
     df = pd.read_csv(request.files.get('file'))
-    return render_template('upload.html', shape=df.shape, columns=df.columns.values)
-
-
-@app.route('/test')
-def test():
-    df0 = pd.DataFrame({'name': ['user01', 'user02', 'user03']})
-    df0.to_sql('users', con=engine)
-    return engine.execute("SELECT * FROM users").fetchall()
-
-
-@app.route('/user_form')
-def new_user():
-    return render_template('user.html')
-
-
-@app.route('/user_info', methods=['POST', 'GET'])
-def user_info():
-    if request.method == 'POST':
-        try:
-            user_email = request.form['user_email']
-            user_name = request.form['user_name']
-
-            with sql.connect("database.db") as con:
-                cur = con.cursor()
-
-                cur.execute("INSERT INTO users (email, name) VALUES (?,?)", (user_email, user_name))
-                msg = "Success"
-
-        except:
-            con.rollback()
-            msg = "error"
-
-        finally:
-            return render_template("result.html", msg=msg)
-            con.close()
-
-
-@app.route('/list')
-def list():
-    con = sql.connect("database.db")  # database.db파일에 접근.
-    con.row_factory = sql.Row
-
-    cur = con.cursor()
-    cur.execute("select * from users")
-
-    rows = cur.fetchall()
-    return render_template("list.html", rows=rows)
+    df.to_sql('csv1', con=engine)
+    return str(engine.execute("SELECT * FROM csv1").fetchall())
+    # return render_template('upload.html', shape=df.shape, columns=df.columns.values)
 
 
 if __name__ == "__main__":
